5ba3353479a144cd0ac31d43210a3adc3a773193
Elijah Odjegba <elijahodjegba@gmail.com>
2022-10-05 14:26:29 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
