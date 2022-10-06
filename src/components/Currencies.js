9d3fbb037ee01ba299a5e4d263216069c14e8385
Elijah Odjegba <elijahodjegba@gmail.com>
2022-10-06 06:44:58 +0100

    Implement currency in currencies

---
+++ src/components/Currencies.js
@@ -0,0 +1,17 @@
+import Currency from "./Currency";
+
+function Currencies() {
+  return (
+    <section className="main-container">
+      <img src="" alt="display-logo" />
+      <div>
+        <h1>
+          The World’s Fastest Growing <br />
+          Crypto App
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default Currencies;
